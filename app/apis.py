0dc1a3954536ea1e24581492284de3215980711a
Avik Datta <avik.datta@imperial.ac.uk>
2022-03-11 12:45:32 +0000

    fix for admin home api

--- app/apis.py
+++ app/apis.py
@@ -3,6 +3,7 @@ from .interop_data_api import SeqrunInteropApi
 from .pre_demultiplexing_data_api import PreDeMultiplexingDataApi
 from .metadata_api import MetadataLoadApi
 from .raw_metadata_api import RawMetadataDataApi
+from .admin_home_api import AdminHomeApi
 
 
 """
@@ -12,3 +13,4 @@ appbuilder.add_api(SeqrunInteropApi)
 appbuilder.add_api(PreDeMultiplexingDataApi)
 appbuilder.add_api(MetadataLoadApi)
 appbuilder.add_api(RawMetadataDataApi)
+appbuilder.add_api(AdminHomeApi)
