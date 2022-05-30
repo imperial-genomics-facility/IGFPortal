b5b8ad4b2bf9995c4dafb990d5c207e099d282bd
Avik Datta <avik.datta@imperial.ac.uk>
2022-05-30 22:25:49 +0100

    updated raw seqrun view

--- app/airflow/airflow_api_utils.py
+++ app/airflow/airflow_api_utils.py
@@ -8,44 +8,45 @@ def post_to_airflow_api(
       data: dict,
       headers: dict = {"Content-Type": "application/json"},
       verify: bool = False):
-  try:
-    with open(airflow_conf_file, "r") as fp:
-      airflow_conf = json.load(fp)
-    if 'url' not in airflow_conf or \
-       'username' not in airflow_conf or \
-       'password' not in airflow_conf:
-      raise KeyError("Missing url, username or password key in conf file")
-    data = json.dumps(data)
-    url = \
-      urljoin(airflow_conf['url'], url_suffix)
-    res = \
-      requests.post(
-        url=url,
-        data=data,
-        headers=headers,
-        auth=(airflow_conf["username"], airflow_conf["password"]),
-        verify=verify)
-    if res.status_code != 200:
-        raise ValueError(
-            f"Failed post request, got status: {res.status_code}")
-    return res
-  except Exception as e:
-    raise ValueError(f"Failed to post to airflow, error: {e}")
+    try:
+        with open(airflow_conf_file, "r") as fp:
+            airflow_conf = json.load(fp)
+        if 'url' not in airflow_conf or \
+           'username' not in airflow_conf or \
+           'password' not in airflow_conf:
+            raise KeyError("Missing url, username or password key in conf file")
+        data = json.dumps(data)
+        url = \
+            urljoin(airflow_conf['url'], url_suffix)
+        res = \
+        requests.post(
+            url=url,
+            data=data,
+            headers=headers,
+            auth=(airflow_conf["username"], airflow_conf["password"]),
+            verify=verify)
+        if res.status_code != 200:
+            raise ValueError(
+                f"Failed post request, got status: {res.status_code}")
+        return res
+    except Exception as e:
+        raise ValueError(f"Failed to post to airflow, error: {e}")
 
 
 def trigger_airflow_pipeline(
       dag_id: str,
       conf_data: dict,
       airflow_conf_file: str):
-  try:
-    url_suffix = \
-      f'dags/{dag_id}/dagRuns'
-    data = {"conf": conf_data}
-    res = \
-      post_to_airflow_api(
-        airflow_conf_file=airflow_conf_file,
-        url_suffix=url_suffix,
-        data=data)
-    return res
-  except Exception as e:
-    raise ValueError(f"Failed to trigger to airflow pipeline {dag_id}, error: {e}")
+    try:
+        url_suffix = \
+            f'dags/{dag_id}/dagRuns'
+        data = {"conf": conf_data}
+        res = \
+        post_to_airflow_api(
+            airflow_conf_file=airflow_conf_file,
+            url_suffix=url_suffix,
+            data=data)
+        return res
+    except Exception as e:
+        raise ValueError(
+                f"Failed to trigger to airflow pipeline {dag_id}, error: {e}")
--- app/raw_seqrun_view.py
+++ app/raw_seqrun_view.py
@@ -14,8 +14,6 @@ def samplesheet_query():
     results = \
         db.session.\
             query(SampleSheetModel).\
-            filter(SampleSheetModel.status=='PASS').\
-            filter(SampleSheetModel.validation_time >= SampleSheetModel.update_time).\
             order_by(SampleSheetModel.samplesheet_id.desc()).\
             limit(20).\
             all()
@@ -23,17 +21,13 @@ def samplesheet_query():
 
 class RawSeqrunView(ModelView):
     datamodel = SQLAInterface(RawSeqrun)
-    label_columns = {
-        "raw_seqrun_igf_id": "Sequencing Id",
-        "status": "Status",
-        "samplesheet": "SampleSheet tag",
-        "date_stamp": "Updated on"
-    }
     list_columns = [
         "raw_seqrun_igf_id",
         "status",
         "samplesheet.samplesheet_tag",
         "samplesheet.status",
+        "samplesheet.validation_time",
+        "samplesheet.update_time",
         "override_cycles",
         "date_stamp"]
     show_columns = [
@@ -43,17 +37,27 @@ class RawSeqrunView(ModelView):
         "date_stamp",
         "samplesheet.samplesheet_tag",
         "samplesheet.status",
+        "samplesheet.validation_time",
+        "samplesheet.update_time",
         "samplesheet.csv_data"]
     label_columns = {
-        "raw_seqrun_igf_id": "Raw seqrun igf id",
+        "raw_seqrun_igf_id": "Sequencing Id",
         "status": "Status",
         "samplesheet.samplesheet_tag": "Samplesheet tag",
         "samplesheet.status": "Samplesheet status",
         "samplesheet.csv_data": "Samplesheet csv data",
+        "samplesheet.validation_time": "Samplesheet validated on",
+        "samplesheet.update_time": "Samplesheet update on",
         "override_cycles": "Override cycles",
         "date_stamp": "Run date"}
-    edit_columns = ["raw_seqrun_igf_id", "samplesheet", "override_cycles"]
-    base_permissions = ["can_list", "can_show", "can_edit"]
+    edit_columns = [
+        "raw_seqrun_igf_id",
+        "samplesheet",
+        "override_cycles"]
+    base_permissions = [
+        "can_list",
+        "can_show",
+        "can_edit"]
     base_order = ("raw_seqrun_igf_id", "desc")
     add_form_extra_fields = {
         "samplesheet": QuerySelectField(
@@ -92,7 +96,6 @@ class RawSeqrunView(ModelView):
             logging.error(e)
         self.update_redirect()
         return redirect(self.get_redirect())
-    """
 
     @action("first_run_demultiplexing", "Run pipeline", confirmation="Confirm pipeline run?", multiple=False, icon="fa-space-shuttle")
     def firts_run_demultiplexing(self, item):
@@ -104,10 +107,11 @@ class RawSeqrunView(ModelView):
         flash("Submitted jobs for {0}".format(', '.join(run_list)), "info")
         self.update_redirect()
         return redirect(self.get_redirect())
+    """
 
 
-    @action("rerun_demultiplexing", "Re-run pipeline", confirmation="All fastq files will be replaced. Confirm pipeline run?", multiple=False, icon="fa-plane")
-    def rerun_demultiplexing(self, item):
+    @action("run_demultiplexing", "Run pipeline", confirmation="Clean up and generate fastqs for run?", multiple=False, icon="fa-plane")
+    def run_demultiplexing(self, item):
         run_list = list()
         if isinstance(item, list):
             run_list = [i.raw_seqrun_igf_id for i in item]
@@ -118,7 +122,7 @@ class RawSeqrunView(ModelView):
         return redirect(self.get_redirect())
 
 
-    @action("trigger_pre_demultiplexing", "Test barcodes", confirmation="confirm pipeline run", icon="fa-rocket")
+    @action("trigger_pre_demultiplexing", "Test barcodes", confirmation="confirm test pipeline run", icon="fa-rocket")
     def trigger_pre_demultiplexing(self, item):
         run_list = list()
         if isinstance(item, list):
