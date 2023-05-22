6b20414ef96c4a7736cd5c82e0433d0c80d67551
Avik Datta <avik.datta@imperial.ac.uk>
2023-05-22 14:42:42 +0100

    aupdated docker  files

---
+++ docker_start_igf_lims_v2.sh
@@ -0,0 +1,2 @@
+#docker-compose -f docker-compose-igfportal_v2.yaml -p igfportal_v2 up -d
+docker-compose -f docker-compose-igfportal_v2_db.yaml -p igfportal_v2 up -d
---
+++ docker_start_igfportal_v2.sh
@@ -0,0 +1,2 @@
+#docker compose -f docker-compose-igfportal_db.yaml -p igfportal up -d
+docker compose -f docker-compose-igfportal.yaml -p igfportal up -d
