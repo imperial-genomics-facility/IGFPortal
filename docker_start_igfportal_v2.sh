ed1d56b1a3ec1f17988e976e1e876b55098e634c
Avik Datta <avik.datta@imperial.ac.uk>
2023-05-22 14:57:13 +0100

    fixed

--- docker_start_igfportal_v2.sh
+++ docker_start_igfportal_v2.sh
@@ -1,2 +1,3 @@
 #docker compose -f docker-compose-igfportal_db.yaml -p igfportal up -d
+# docker exec -i portal_db mysql --host=portal_db  --user=root --password igfportaldb < backup.sql
 docker compose -f docker-compose-igfportal.yaml -p igfportal up -d
