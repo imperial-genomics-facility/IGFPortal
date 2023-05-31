be73d7032fb6c4a544578bdc644dd92058b47bfd
Avik Datta <avik.datta@imperial.ac.uk>
2023-05-31 15:33:51 +0100

    updated docker settings

--- docker_start_igf_lims_v2.sh
+++ docker_start_igf_lims_v2.sh
@@ -1,2 +1,2 @@
 #docker-compose -f docker-compose-igf-lims_v2.yaml -p igfportal_v2 up -d
-docker-compose -f docker-compose-igf-lims_v2_db.yaml -p igfportal_v2 up -d
+UID="$(id -u)" GID="$(id -g)"  docker-compose -f docker-compose-igf-lims_v2_db.yaml -p igfportal_v2 up -d
