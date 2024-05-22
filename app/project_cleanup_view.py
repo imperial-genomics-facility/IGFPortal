e5d8f36954cc4060e239951f931cab561259fac4
Avik Datta <avik.datta@imperial.ac.uk>
2024-05-22 14:49:05 +0100

    updates for db update

--- app/project_cleanup_view.py
+++ app/project_cleanup_view.py
@@ -269,7 +269,7 @@ class ProjectCleanupPendingView(ModelView):
                         apply_async(args=[airflow_dag_id, entry_list, g.user.id, True])
                 ## mark entries as PROCESSING to prevent repeat runs
                 update_status_for_project_cleanup(
-                    project_cleanup_id_list=entry_list,
+                    project_cleanup_id_list=[k for row in entry_list for k in row],
                     status='PROCESSING',
                     user_id=g.user.id)
                 flash("Submitted DB cleanup task", "info")
