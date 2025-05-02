7ec8e4ace6d6705f0bc46373dbcbd319c2596136
Avik Datta <avik.datta@imperial.ac.uk>
2025-05-02 13:29:08 +0100

    fix for cosmx data model

--- app/models.py
+++ app/models.py
@@ -199,7 +199,7 @@ class RawMetadataModel(Model):
 class RawCosMxMetadataModel(Model):
   __tablename__ = 'raw_cosmx_metadata_entry'
   __table_args__ = (
-    UniqueConstraint('metadata_tag'),
+    UniqueConstraint('cosmx_metadata_tag'),
     { 'mysql_engine':'InnoDB', 'mysql_charset':'utf8'  })
   raw_cosmx_metadata_id = Column(INTEGER(unsigned=True), primary_key=True, nullable=False)
   cosmx_metadata_tag = Column(String(80), nullable=False)                                     # 2 ^ 24
