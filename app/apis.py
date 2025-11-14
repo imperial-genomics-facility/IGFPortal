5f0911bc76a81f6215fc805316845b42d4ea2c76
Avik Datta <avik.datta@imperial.ac.uk>
2025-11-14 12:44:14 +0000

    commented old api codes

--- app/apis.py
+++ app/apis.py
@@ -1,11 +1,11 @@
 from . import appbuilder
 from .interop_data_api import SeqrunInteropApi
 from .pre_demultiplexing_data_api import PreDeMultiplexingDataApi
-from .metadata_api import MetadataLoadApi
+# from .metadata_api import MetadataLoadApi
 from .raw_metadata_api import RawMetadataDataApi
 from .admin_home_api import AdminHomeApi
 from .raw_seqrun_api import RawSeqrunApi
-from .raw_analysis_api import RawAnalysisApi
+# from .raw_analysis_api import RawAnalysisApi
 from .raw_analysis_api_v2 import RawAnalysisV2Api
 from .project_cleanup_api import ProjectCleanupApi
 
@@ -15,10 +15,10 @@ from .project_cleanup_api import ProjectCleanupApi
 """
 appbuilder.add_api(SeqrunInteropApi)
 appbuilder.add_api(PreDeMultiplexingDataApi)
-appbuilder.add_api(MetadataLoadApi)
+# appbuilder.add_api(MetadataLoadApi)
 appbuilder.add_api(RawMetadataDataApi)
 appbuilder.add_api(AdminHomeApi)
 appbuilder.add_api(RawSeqrunApi)
-appbuilder.add_api(RawAnalysisApi)
+# appbuilder.add_api(RawAnalysisApi)
 appbuilder.add_api(RawAnalysisV2Api)
 appbuilder.add_api(ProjectCleanupApi)
