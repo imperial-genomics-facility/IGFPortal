606620588f68069976c030bbbe6670183a93b739
Avik Datta <avik.datta@imperial.ac.uk>
2025-12-09 13:59:26 +0000

    added metadata builder view

---
+++ app/cosmx_raw_metadata_view.py
@@ -0,0 +1,97 @@
+import os
+import logging
+from typing import Tuple, Any
+from app import db, celery
+from flask_appbuilder.actions import action
+from wtforms_sqlalchemy.fields import QuerySelectField
+from flask_appbuilder.fieldwidgets import Select2Widget
+from flask_appbuilder.models.sqla.filters import FilterInFunction
+from app.models import (
+    RawCosMxMetadataBuilder,
+    RawCosMxMetadataModel)
+from flask_appbuilder import ModelView
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask import redirect, flash, url_for, send_file
+from app.asyncio_util import run_async
+from app.file_download_util import prepare_file_for_download
+from .airflow.airflow_api_utils import trigger_airflow_pipeline
+from .airflow.airflow_api_utils import get_airflow_dag_id
+from app.cosmx_metadata.cosmx_metadata_utils import (
+    check_user_data_validation,
+    raw_user_query
+)
+
+
+log = logging.getLogger(__name__)
+
+COSMX_METADATA_REGISTRATION_DAG_TAG = 'cosmx_metadata_registration_dag'
+
+@celery.task(bind=True)
+def async_validate_and_register_cosmx_metadata(self, id_list):
+    try:
+        results = list()
+        return dict(zip(id_list, results))
+    except Exception as e:
+        log.error(
+            f"Failed to run celery job, error: {e}")
+
+
+def action_validate_and_register_cosmx_metadata(
+    item: list[RawCosMxMetadataBuilder] | RawCosMxMetadataBuilder) \
+        -> Tuple[list[str], dict[int, Any]]:
+    try:
+        id_list = list()
+        project_list = list()
+        if item is None:
+            raise ValueError("No item found")
+        if isinstance(item, list):
+            id_list = [i.raw_cosmx_metadata_builder_id for i in item]
+            project_list = [i.cosmx_metadata_tag for i in item]
+        else:
+            id_list = [item.raw_cosmx_metadata_builder_id]
+            project_list = [item.cosmx_metadata_tag]
+        response_dict = \
+            async_validate_and_register_cosmx_metadata.\
+                apply_async(args=[id_list])
+        log.info(
+            f"Submitted schema validation job, status: {response_dict}")
+        return project_list, response_dict
+    except Exception as e:
+        raise ValueError(
+            f"Failed to run action for json validation, error: {e}")
+
+class RawCosMxMetadataBuilderView(ModelView):
+    datamodel = SQLAInterface(RawCosMxMetadataBuilder)
+    label_columns = {
+        "pipeline.pipeline_name": "Pipeline name",
+        "cosmx_metadata_tag": "Project name",
+        "name": "Full name",
+        "email_id": "Email",
+        "username": "Globus username",
+        "raw_user.name": "Existing user",
+        "report": "Validation report",
+    }
+    base_permissions = [
+        "can_list",
+        "can_show",
+        "can_add",
+        "can_edit",
+        "can_delete"]
+    base_order = ("raw_cosmx_metadata_builder_id", "desc")
+    base_filters = [
+        ["status", FilterInFunction, lambda: ["UNKNOWN", "FAILED"]]]
+
+    add_form_extra_fields = {
+        "raw_user": QuerySelectField(
+            "RawIgfUser",
+            query_factory=raw_user_query,
+            widget=Select2Widget()
+        ),
+    }
+    edit_form_extra_fields = {
+        "raw_user": QuerySelectField(
+            "RawIgfUser",
+            query_factory=raw_user_query,
+            widget=Select2Widget()
+        )
+    }
